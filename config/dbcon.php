0f47db352b95dd5b0d007256d0e734ad4cca4fab
Chingfu20 <jarinaangelo36@gmail.com>
2024-07-21 19:43:52 +0800

    ss

--- config/dbcon.php
+++ config/dbcon.php
@@ -3,7 +3,7 @@
 define('DB_SERVER',"localhost");
 define('DB_USERNAME',"u510162695_db_jisu_pos");
 define('DB_PASSWORD',"1Db_jisu_pos");
-define('DB_DATABASE',"db_jisu_pos");
+define('DB_DATABASE',"u510162695_db_jisu_pos");
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
@@ -12,4 +12,3 @@ if(!$conn){
 }
 
 ?>
-    
