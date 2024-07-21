7b3e3b2aa3c6210c8a7d10807691dd476a86a71f
Chingfu20 <jarinaangelo36@gmail.com>
2024-07-21 19:36:04 +0800

    Update dbcon.php

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_SERVER',"localhost");
-define('DB_USERNAME',"root");
-define('DB_PASSWORD',"");
+define('DB_USERNAME',"u510162695_db_jisu_pos");
+define('DB_PASSWORD',"1Db_jisu_pos");
 define('DB_DATABASE',"db_jisu_pos");
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
@@ -12,3 +12,4 @@ if(!$conn){
 }
 
 ?>
+    
